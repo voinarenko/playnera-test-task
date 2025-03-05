57208c31aa16e3ab1a46f11a47d0a009a0fce411
Sergei Voinarenko <voynarenko@gmail.com>
2025-03-05 14:22:28 +0300

    Input service updated.

--- Assets/Code/Gameplay/Input/Service/InputService.cs
+++ Assets/Code/Gameplay/Input/Service/InputService.cs
@@ -1,16 +1,28 @@
+using UnityEngine.InputSystem;
+
 namespace Code.Gameplay.Input.Service
 {
   public class InputService : IInputService
   {
     private readonly PlayerInputActions _controls = new();
 
-    public void Enable() =>
+    public void Enable()
+    {
       _controls.Enable();
+      ActivateTouch();
+    }
 
     public PlayerInputActions GetActions() =>
       _controls;
 
     public void Disable() =>
       _controls.Disable();
+
+    private void ActivateTouch()
+    {
+      InputSystem.settings.updateMode = InputSettings.UpdateMode.ProcessEventsInFixedUpdate;
+      Touchscreen.current?.MakeCurrent();
+      InputSystem.EnableDevice(Touchscreen.current);
+    }
   }
 }
